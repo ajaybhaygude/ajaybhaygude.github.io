cb04f472582fb26948f48fe96c2a67ec38f83e47
ajaybhaygude <93912438+ajaybhaygude@users.noreply.github.com>
2024-07-14 22:33:42 +0530

    Update Music_Store_Query.sql
    new commit

--- Music_Store_Query.sql
+++ Music_Store_Query.sql
@@ -112,7 +112,7 @@ ORDER BY 5 DESC;
 
 
 
--- Q3: Write a query that determines the customer that has spent the most on music for each country. 
+-- Q10: Write a query that determines the customer that has spent the most on music for each country. 
 -- Write a query that returns the country along with the top customer and how much they spent. 
 -- For countries where the top amount spent is shared, provide all customers who spent this amount. 
 
